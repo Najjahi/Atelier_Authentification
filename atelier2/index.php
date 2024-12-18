cfc1a9f5aacee16f03c618a071b01c98bfce70e6
Najjahi <157368284+Najjahi@users.noreply.github.com>
2024-12-18 15:22:12 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,34 +1,39 @@
 <?php
 // Démarrer une session utilisateur
-// Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
 
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === 'secret') {
 if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === '12345') {
+    header('Location: home.php');
     header('Location: page_admin.php');
     exit();
 }
 
-// Gérer la soumission du formulaire
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Vérification simple du username et de son password.
+@@ -19,7 +19,7 @@
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', '12345', time() + 3600, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        $token = bin2hex(random_bytes(16));
+        setcookie('authToken', 'admin_' . $token, time() + 60, '/', '', true, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes)
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
+@@ -36,15 +36,15 @@
+</head>
+<body>
+    <h1>Atelier authentification par Cookie</h1>
+    <h3>La page <a href="home.php">home.php</a> est inaccéssible tant que vous ne vous serez pas connecté avec le login 'admin' et mot de passe 'secret'</h3>
+    <h3>La page <a href="page_admin.php">page_admin.php</a> est inaccéssible tant que vous ne vous serez pas connecté avec le login 'admin' et mot de passe 'secret'</h3>
+    <form method="POST" action="">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" required>
+        <br><br>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+        <button type="submit">Se connecter</button>
+    </form>
+</body>
+</html>

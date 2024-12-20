7f5532c13b0954f8c87cb172c6268ac44e10b0b9
Najjahi <157368284+Najjahi@users.noreply.github.com>
2024-12-20 11:30:26 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -3,6 +3,10 @@
 session_start();
 
 // Vérifier si l'utilisateur est déjà connecté
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true || $_SESSION['username'] !== 'admin') {
+     else {
+        $error = "Nom d'utilisateur ou mot de passe incorrect.";
+    }
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['username']=== 'admin') {
     header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
     exit();

34c5869035a0ced42446c81d76c26eacec76265d
Najjahi <157368284+Najjahi@users.noreply.github.com>
2024-12-20 12:31:22 +0100

    Update index.php

--- atelier4/index.php
+++ atelier4/index.php
@@ -2,6 +2,8 @@
 // Nom d'utilisateur et mot de passe corrects
 $valid_username = 'admin';
 $valid_password = 'secret';
+$valid_username = 'user';
+$valid_password = 'utilisateur';
 
 // Vérifier si l'utilisateur a envoyé des identifiants
 if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
@@ -11,11 +13,26 @@ if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
     echo 'Vous devez entrer un nom d\'utilisateur et un mot de passe pour accéder à cette page.';
     exit;
 }
+else (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
+    // Envoyer un header HTTP pour demander les informations
+    header('WWW-Authenticate: Basic "');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Vous devez entrer un nom d\'utilisateur et un mot de passe pour accéder à cette page.';
+    exit;
+}
+}
 
 // Vérifier les identifiants envoyés
 if ($_SERVER['PHP_AUTH_USER'] !== $valid_username || $_SERVER['PHP_AUTH_PW'] !== $valid_password) {
     // Si les identifiants sont incorrects
     header('WWW-Authenticate: Basic realm="Zone Protégée"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Nom d\'utilisateur ou mot de passe incorrect.';
+    exit;
+}
+else ($_SERVER['PHP_AUTH_USER'] !== $valid_username || $_SERVER['PHP_AUTH_PW'] !== $valid_password) {
+    // Si les identifiants sont incorrects
+    header('WWW-Authenticate: Basic ');
     header('HTTP/1.0 401 Unauthorized');
     echo 'Nom d\'utilisateur ou mot de passe incorrect.';
     exit;

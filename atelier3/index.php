df2dc04e17d920243b74c578db0a28bd9426c84c
Najjahi <157368284+Najjahi@users.noreply.github.com>
2024-12-20 12:10:23 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -50,8 +50,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
    
     $_SESSION['visites'] = 1;
        $error = "Vous avez visiter cette page ".$_SESSION['visites']."fois";
-        echo $_SESSION['visites'];
-        <?php echo htmlspecialchars($_SESSION['visites']); ?>
+        echo htmlspecialchars($_SESSION['visites']);
+     
 }
 
 }

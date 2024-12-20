e465bf6d7107d8cc9538bfeac92f230cfeb5294e
Najjahi <157368284+Najjahi@users.noreply.github.com>
2024-12-20 11:01:17 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['
     header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
     exit();
 } else {
-    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true  && $_SESSION['username']=== 'user') ) {
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true  && $_SESSION['username']=== 'user') {
     header('Location: page_user.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
     exit();
 } 
